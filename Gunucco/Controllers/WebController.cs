71b5c77a8ef0e8b2d005c913719b7630ab7838e4
KMY <tt@kmycode.net>
2017-05-27 17:13:33 +0900

    #9 Add sign in page

---
+++ Gunucco/Controllers/WebController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gunucco.Controllers
+{
+    [Route("/web")]
+    public class WebController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("signup")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
