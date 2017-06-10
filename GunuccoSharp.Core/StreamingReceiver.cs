4caa704ff655c183b0600e80aa834f9b9108945c
KMY <tt@kmycode.net>
2017-06-10 13:28:42 +0900

    Remove debug code

--- GunuccoSharp.Core/StreamingReceiver.cs
+++ GunuccoSharp.Core/StreamingReceiver.cs
@@ -77,67 +77,31 @@ namespace GunuccoSharp
             this.stream = newStream;
             this.isConnecting = true;
 
-            Queue<string> lineBuffer = new Queue<string>();
-            string lastLine = "";
-            var buf = new char[4096];
-            string lastJson;
-
             using (var reader = new StreamReader(stream))
             {
-                var bufferReader = new StreamReaderBuffer
-                {
-                    Stream = reader,
-                };
-                bufferReader.Start();
-
                 while (this.isConnecting)
                 {
                     try
                     {
-                        string allText = lastLine + bufferReader.Read();
-
-                        lastLine = "";
-                        var lines = allText.Split('\n');
-                        for (int i = 0; i < lines.Length; i++)
+                        var line = await reader.ReadLineAsync();
+                        if (line.StartsWith("data: "))
                         {
-                            var l = lines[i];
-                            if (!string.IsNullOrEmpty(l))
+                            var json = line.Substring("data: ".Length);
+
+                            // check error
+                            var err = JsonConvert.DeserializeObject<ApiMessage>(json);
+                            if (err.StatusCode != default(int))
                             {
-                                if (l.EndsWith("}") && (allText.EndsWith("\n") || i < lines.Length - 1))
-                                {
-                                    lineBuffer.Enqueue(l);
-                                }
-                                else
-                                {
-                                    lastLine = l;
-                                }
+                                throw new GunuccoErrorException(err);
                             }
-                            System.Diagnostics.Debug.WriteLine(l);
-                        }
 
-                        while (lineBuffer.Count > 0)
-                        {
-                            var line = lineBuffer.Dequeue();
-                            if (line.StartsWith("data: "))
+                            else
                             {
-                                var json = line.Substring("data: ".Length);
-                                lastJson = json;
+                                // deserialize object
+                                var obj = JsonConvert.DeserializeObject<T>(json);
 
-                                // check error
-                                var err = JsonConvert.DeserializeObject<ApiMessage>(json);
-                                if (err.StatusCode != default(int))
-                                {
-                                    throw new GunuccoErrorException(err);
-                                }
-
-                                else
-                                {
-                                    // deserialize object
-                                    var obj = JsonConvert.DeserializeObject<T>(json);
-
-                                    // send object
-                                    this.receiver.OnNext(obj);
-                                }
+                                // send object
+                                this.receiver.OnNext(obj);
                             }
                         }
                     }
@@ -149,59 +113,6 @@ namespace GunuccoSharp
                         }
                     }
                 }
-
-                bufferReader.Exit();
-            }
-        }
-
-        private class StreamReaderBuffer
-        {
-            public StreamReader Stream { get; set; }
-            private char[] buffer = new char[4096];
-            private int index = 0;
-            private bool isStop = false;
-            private bool isExit = false;
-
-            public string Read()
-            {
-                this.isStop = true;
-
-                var buf = this.buffer;
-                this.buffer = new char[4096];
-                var text = new string(buf).Substring(0, this.index);
-
-                this.index = 0;
-                this.isStop = false;
-
-                return text;
-            }
-
-            public void Start()
-            {
-                Task.Run(() =>
-                {
-                    try
-                    {
-                        while (!this.isExit)
-                        {
-                            while (this.isStop) ;
-                            var c = (char)this.Stream.Read();
-                            if (c != 0)
-                            {
-                                this.buffer[this.index++] = c;
-                            }
-                        }
-                    }
-                    catch
-                    {
-                        this.Exit();
-                    }
-                });
-            }
-
-            public void Exit()
-            {
-                this.isExit = true;
             }
         }
 
